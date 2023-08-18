b329ba77e8179408201bffbbaf90d673f531c7f0
dpadilla <didacpadilla20@gmail.com>
2023-08-18 16:56:34 +0200

    Added arrow control on images

--- web/src/Pages/Images.tsx
+++ web/src/Pages/Images.tsx
@@ -1,8 +1,8 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { useHouseById } from "../Hooks/Houses"
 import { CenterLoading } from "../Components/CenterLoading"
 import { WrongMessage } from "../Components/WrongMessage"
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { Stack, Typography } from "@mui/material"
 import { useMobile } from "../Hooks/Mobile"
 
@@ -15,9 +15,10 @@ export const ImagesPage = () => {
   const { id } = useParams()
   const house = useHouseById(parseInt(id ?? '0'))
   const isMobile = useMobile()
+  const navigate = useNavigate()
   const [imageIndex, setImageIndex] = useState(0)
 
-  const changeImage = (direction: Direction) => {
+  const changeImage = useCallback((direction: Direction) => {
     if (!house.data) return
     switch (direction) {
       case Direction.Forward:
@@ -29,7 +30,21 @@ export const ImagesPage = () => {
         else setImageIndex(imageIndex - 1)
         break
     }
-  }
+  }, [house.data, imageIndex])
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === 'ArrowRight') changeImage(Direction.Forward)
+      if (event.key === 'ArrowLeft') changeImage(Direction.Backward)
+      if (event.key === 'Escape') navigate(-1)
+    }
+
+    window.addEventListener('keydown', handleKeyPress)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress)
+    }
+  }, [changeImage, navigate])
 
   if (house.loading) return <CenterLoading label="Loading House..." />
   if (!house.data || house.error) return <WrongMessage message={`La casa con id ${id} no existe...`} />
@@ -49,11 +64,14 @@ export const ImagesPage = () => {
         </Stack>
       }
       {!isMobile &&
-        <Stack
-          height='calc(100vh - 4rem)'
-          overflow='hidden'
-        >
-          <img alt='Imagen de la casa' src={house.data.images[imageIndex]} style={{ height: '100%', objectFit: 'contain' }} />
+        <Stack alignItems='center' gap='.5rem'>
+          <Stack
+            height='calc(95vh - 4rem)'
+            overflow='hidden'
+          >
+            <img alt='Imagen de la casa' src={house.data.images[imageIndex]} style={{ height: '100%', objectFit: 'contain' }} />
+          </Stack>
+          <Typography variant="body1">Foto {imageIndex + 1}/{house.data.images.length}</Typography>
         </Stack>
       }
       <div style={{ width: '49vw', height: '100%', position: 'absolute', top: '0' }} onClick={() => changeImage(Direction.Backward)} />
