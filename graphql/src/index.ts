8eebef62fd49ba10f0d5231ad881a6186fcc8cf9
dpadilla <didacpadilla20@gmail.com>
2023-08-21 12:38:44 +0200

    Fixed get schema

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -2,8 +2,9 @@ import { resolvers } from './resolvers';
 import { ApolloServer, BaseContext } from '@apollo/server';
 import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
 import { readFileSync } from 'fs'
+import * as path from 'path'
 
-export const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' })
+export const typeDefs = readFileSync(path.join(__dirname, './schema.graphql'), { encoding: 'utf-8' })
 
 const server = new ApolloServer<BaseContext>({
     typeDefs,
